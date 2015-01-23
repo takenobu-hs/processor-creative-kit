c2becd94d736dfd5262d3c583de5eea8ae1244d0
Takenobu T <takenobu.hs@gmail.com>
2015-01-23 23:24:30 +0900

    modify default register trace

--- examples/trace.hs
+++ examples/trace.hs
@@ -16,7 +16,8 @@ import Language.Pck.Tool
 main :: IO ()
 main = do (file:_) <- getArgs
           insts <- parseInstFile file
-          runDbgIO [TrcInst] []  [(0, insts)] []
+          runDbgIO [TrcInst, TrcReg] []  [(0, insts)] []
+--        runDbgIO [TrcInst]         []  [(0, insts)] []
 
 
 
