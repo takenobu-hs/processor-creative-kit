c1b715bc3d8203ce34122819acd08d3e48d2662c
Takenobu T <takenobu.hs@gmail.com>
2015-01-26 21:31:46 +0900

    rename file

---
+++ examples/break.hs
@@ -0,0 +1,24 @@
+
+{-
+  brake example:
+
+    $ runhaskell examples/brake.hs  examples/test0.asm
+-}
+
+module Main where
+
+import System.Environment(getArgs)
+import Language.Pck.Cpu
+import Language.Pck.Tool
+
+
+main :: IO ()
+main = do (file:_) <- getArgs
+          insts <- parseInstFile file
+          runDbgIO [TrcInst] [(BrkPc BEQ 2)]      [(0, insts)] []
+--        runDbgIO [TrcInst] [(BrkGReg R1 BGT 2)] [(0, insts)] []
+--        runDbgIO [TrcInst] [(BrkDmem 0 BGT 0)]  [(0, insts)] []
+--        runDbgIO [TrcInst] [(BrkPc BEQ 2), (BrkPc BEQ 5)] [(0, insts)] []
+
+
+
